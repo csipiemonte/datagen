4015bfd81f464e4833c414cabaf17e76c904858a
ANTONACI Mauro 869 <mauro.antonaci@csi.it>
2010-04-22 14:20:24 +0000

    STDMDD-505: - aggiunta gestione del package personalizzato (property basePackage nel basicDao.oaw) per la generazioen in progetti guigen o servicegen - modifica della property daoBeansFilePosition in daoBeansFileName: ora occorre indicare anche il nome del file (necessario in caso si debbano generare due moduli dao nello stesso progetto => servono file separati per evitare conflitti di classloader - (prime bozze di QBE)
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/datagen/trunk@1425 be04b910-fd5c-0410-891f-bca81b977169

--- src/it/csi/mddtools/datagen/genutils/GenUtils.java
+++ src/it/csi/mddtools/datagen/genutils/GenUtils.java
@@ -1,5 +1,7 @@
 package it.csi.mddtools.datagen.genutils;
 
+import java.util.Properties;
+
 public class GenUtils {
 	
 	/**
@@ -14,5 +16,42 @@ public class GenUtils {
 	public static String getRegionUID(String sourceId){
 		String uid = "R"+sourceId.hashCode();
 		return uid;
+	}
+	
+	public static String pkg2Path(String pkg){
+		System.out.println("from:"+pkg);
+		String path= pkg.replaceAll("\\p{Punct}", "/"); 
+		System.out.println("to:"+path);
+		return path;
+	}
+	
+	public static void dumpProperties(){
+		org.openarchitectureware.util.stdlib.PropertiesExtension.getProperties().list(System.out);
+	}
+	
+	public static void backupProperties(){
+		System.out.println("Backup properties originarie");
+		Properties orig = org.openarchitectureware.util.stdlib.PropertiesExtension.getProperties();
+		Properties bkpProps = new Properties();
+		bkpProps.putAll(orig);
+		_origProps = bkpProps;
+		
+	}
+	
+	public static void restoreProperties(){
+		System.out.println("Restore properties originarie");
+		if (_origProps!=null){
+			org.openarchitectureware.util.stdlib.PropertiesExtension.setProperties(_origProps);	
+		}
+		_origProps = null;
+		
+	}
+	
+	static Properties _origProps=null;
+	
+	public static void setProperty(String name, String val){
+		java.util.Properties props = org.openarchitectureware.util.stdlib.PropertiesExtension.getProperties();
+		props.put(name, val);
+		org.openarchitectureware.util.stdlib.PropertiesExtension.setProperties(props);
 	}
 }
