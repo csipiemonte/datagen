4e115c64543777fc71b1065ad2cdff7258a18f2e
ANTONACI Mauro 869 <mauro.antonaci@csi.it>
2010-08-27 10:27:01 +0000

    STDMDD-505: mini bugfix (generated not mancante)
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/datagen/trunk@1882 be04b910-fd5c-0410-891f-bca81b977169

--- src/it/csi/mddtools/datagen/provider/OrderSpecItemProvider.java
+++ src/it/csi/mddtools/datagen/provider/OrderSpecItemProvider.java
@@ -160,7 +160,7 @@ public class OrderSpecItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
