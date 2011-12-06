d53eb3a711be431040da9465df1b5796b9f07b3d
MANFROI Andrea 71895 <andrea.manfroi@consulenti.csi.it>
2011-12-06 16:22:32 +0000

    STDMDD-510: modificato l'item provider delle included columns in maniera tale che valorizzi come possibili scelte solo le colonne della tabella corrente e non tutte quelle dello schema.
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/datagen/trunk@3889 be04b910-fd5c-0410-891f-bca81b977169

--- src/it/csi/mddtools/rdbmdl/constraints/provider/ColumnRefConstraintItemProvider.java
+++ src/it/csi/mddtools/rdbmdl/constraints/provider/ColumnRefConstraintItemProvider.java
@@ -21,14 +21,32 @@
 package it.csi.mddtools.rdbmdl.constraints.provider;
 
 
+import it.csi.mddtools.datagen.DataAccessObject;
+import it.csi.mddtools.datagen.DatagenPackage;
+import it.csi.mddtools.datagen.LookupResolver;
+import it.csi.mddtools.datagen.LookupResolvers;
+import it.csi.mddtools.rdbmdl.RdbmdlPackage;
+import it.csi.mddtools.rdbmdl.Table;
+import it.csi.mddtools.rdbmdl.TableColumn;
 import it.csi.mddtools.rdbmdl.constraints.ColumnRefConstraint;
 import it.csi.mddtools.rdbmdl.constraints.ConstraintsPackage;
+import it.csi.mddtools.rdbmdl.constraints.ForeignKey;
+import it.csi.mddtools.rdbmdl.constraints.PrimaryKey;
+import it.csi.mddtools.rdbmdl.constraints.impl.CheckConstraintImpl;
+import it.csi.mddtools.rdbmdl.constraints.impl.ColumnRefConstraintImpl;
+import it.csi.mddtools.rdbmdl.constraints.impl.ForeignKeyImpl;
+import it.csi.mddtools.rdbmdl.constraints.impl.PrimaryKeyImpl;
+import it.csi.mddtools.rdbmdl.constraints.impl.UniqueConstraintImpl;
+import it.csi.mddtools.rdbmdl.impl.TableImpl;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
 
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
@@ -37,6 +55,7 @@ import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 
 /**
  * This is the item provider adapter for a {@link it.csi.mddtools.rdbmdl.constraints.ColumnRefConstraint} object.
@@ -82,22 +101,43 @@ public class ColumnRefConstraintItemProvider
 	 * This adds a property descriptor for the Included Columns feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	protected void addIncludedColumnsPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_ColumnRefConstraint_includedColumns_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_ColumnRefConstraint_includedColumns_feature", "_UI_ColumnRefConstraint_type"),
-				 ConstraintsPackage.Literals.COLUMN_REF_CONSTRAINT__INCLUDED_COLUMNS,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+//		itemPropertyDescriptors.add
+//			(createItemPropertyDescriptor
+//				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+//				 getResourceLocator(),
+//				 getString("_UI_ColumnRefConstraint_includedColumns_feature"),
+//				 getString("_UI_PropertyDescriptor_description", "_UI_ColumnRefConstraint_includedColumns_feature", "_UI_ColumnRefConstraint_type"),
+//				 ConstraintsPackage.Literals.COLUMN_REF_CONSTRAINT__INCLUDED_COLUMNS,
+//				 true,
+//				 false,
+//				 true,
+//				 null,
+//				 null,
+//				 null));
+		itemPropertyDescriptors.add(new ItemPropertyDescriptor(
+				((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+				getString("_UI_ColumnRefConstraint_includedColumns_feature"), 
+				getString("_UI_PropertyDescriptor_description",
+						  "_UI_ColumnRefConstraint_includedColumns_feature",
+						  "_UI_ColumnRefConstraint_type"),
+				ConstraintsPackage.eINSTANCE.getColumnRefConstraint_IncludedColumns(),
+				true) {
+					@SuppressWarnings("unchecked")
+					protected Collection getComboBoxObjects(Object object) {
+						ArrayList<TableColumn> result = new ArrayList<TableColumn>();
+						ColumnRefConstraintImpl crci = (ColumnRefConstraintImpl)object;
+						TableImpl ti = (TableImpl) crci.eContainer();
+						for(int i = 0; i<ti.getColumns().size();i++){
+							result.add(ti.getColumns().get(i));
+							}
+						
+					return result;						
+					}
+				}
+			);
 	}
 
 	/**
